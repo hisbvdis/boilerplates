cb82687b4b21c82f5eafa2ecd5d4c79e9d42d831
hisbvdis <hisbvdis@gmail.com>
2022-05-13 12:58:14 +0300

    Chages

--- services/svg-minificator-sprite/gulpfile.js
+++ services/svg-minificator-sprite/gulpfile.js
@@ -1,61 +1,74 @@
-// Install packages:
+// =============================================================================
+// INSTALLED PACKAGES
+// =============================================================================
+// npm i -g gulp-cli
 // npm i -D gulp browser-sync dev-ip
 // npm i -D gulp-svgmin gulp-svgstore
 // npm i -D gulp-rename
 
-// Commands:
+
+
+// =============================================================================
+// COMMANDS
+// =============================================================================
 // - gulp: Run dev server
 
-// Packages
-// service
+
+
+// =============================================================================
+// PACKAGES
+// =============================================================================
+// SERVICE
 const gulp = require("gulp");
 const browsersync = require("browser-sync").create();
 const devip = require("dev-ip");
-// images
+
+// IMAGES (SVG)
 const svgmin = require("gulp-svgmin");
 const svgstore = require("gulp-svgstore");
-// files
+
+// FILES
 const rename = require("gulp-rename");
 
 
 
-// ===================================
+// =============================================================================
 // SERVER
-// ===================================
+// =============================================================================
 // Dev server
 gulp.task("default", function() {
   browsersync.init({
     server: "src",
-    host: devip(), // From "dev-ip" extension
+    host: devip(),
     reloadDelay: 0,
     reloadDebounce: 100,
     notify: false,
   });
 
-  gulp.watch("src/svg-1-inbox/*.svg").on("add", gulp.series("svgmin"));
-  gulp.watch("src/svg-3-sprite/").on("all", gulp.series("svg-sprite"));
+  gulp.watch("src/img/svg-1-origin/*.svg").on("add", gulp.series("svg-min"));
+  gulp.watch("src/img/svg-3-for-sprite/").on("all", gulp.series("svg-sprite"));
 });
 
 
 
-// ===================================
-// DEV TASKS
-// ===================================
+// =================================================================
+// TASKS (DEV)
+// =================================================================
 // SVG minification
-gulp.task("svgmin", function() {
+gulp.task("svg-min", function() {
   return gulp
-    .src("src/svg-1-inbox/*.svg")
+    .src("src/img/svg-1-origin/*.svg")
     .pipe(svgmin({
       plugins: [{ name: 'removeViewBox', active: false }]
     }))
-    .pipe(gulp.dest("src/svg-2-minified"));
+    .pipe(gulp.dest("src/img/svg-2-minified"));
 });
 
 // SVG sprite-generator
 gulp.task("svg-sprite", function() {
   return gulp
-    .src("src/svg-3-sprite/*.svg")
+    .src("src/img/svg-3-for-sprite/*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("src/"));
+    .pipe(gulp.dest("src/img/"));
 });
